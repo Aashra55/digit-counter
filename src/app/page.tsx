dc6c7d5ce9b1ac0f62a262beced04bddacce44a9
Aashra <saleemlatif17@gmail.com>
2024-11-12 21:15:22 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home(){
         <div className="w-[40%] rounded-2xl h-[70%] bg-pink-100 flex flex-col
          gap-[10%] justify-center items-center p-[20px] gap-[5%]">
           <h1 className="text-[50px] h-[10%]">Counter</h1>
-          <div className="flex flex-col w-[100%] justify-center items-center h-[80%] gap-[10%]">
+          <div className="flex flex-col w-[100%] justify-center items-center h-[80%] gap-[5%]">
           <p className="text-[100px]">{counter}</p>
           <div className="flex flex-row w-[50%] justify-around items-center">
           <button onClick={()=>setCounter(counter+1)} className="
